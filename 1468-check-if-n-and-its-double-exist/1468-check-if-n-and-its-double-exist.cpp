74ce04a6f1e255b807fe9da2dbe1692d8e76d29e
Piyush Pal <168671044+piyushpal123@users.noreply.github.com>
2024-12-01 15:19:59 +0530

    Time: 0 ms (100%), Space: 13.5 MB (17.49%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_map<int,int>mpp;
         for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]=i;
+            for(int j=0;j<arr.size();j++){
+                if(i!=j&&arr[i]==2*arr[j])return true;
+            }
         }
-        for(int i=0;i<arr.size();i++){
-            int value=2*arr[i];
-            if(mpp.find(value)!=mpp.end()&&mpp[value]!=i)return true;
-        }
-
      return false;   
     }
 };
